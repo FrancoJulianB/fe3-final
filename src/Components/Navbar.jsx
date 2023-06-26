30f8f213dcb9eda346038e7e164b5fda03fc6928
AbigailJustiniano <70248439+debora2004@users.noreply.github.com>
2023-06-26 20:48:56 -0300

    [FEAT] Navbar and Routes without styles

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
 
   return (
     <nav>
+      <ul>
+       
+          <li>
+          <Link to="/contact">Contact</Link>
+          <Link to='/details'>Details</Link>
+          <Link to='/favs'>Favs</Link>
+          <Link to='/home'>Home</Link>
+          </li>       
+        
+      </ul>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
       <button>Change theme</button>
