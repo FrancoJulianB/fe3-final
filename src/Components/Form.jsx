a065d8ac2f371959b8ee8802a7e0c31bea621aa0
AbigailJustiniano <70248439+debora2004@users.noreply.github.com>
2023-06-26 22:47:22 -0300

    [FEAT] Form con validaciones y estilos (basicos)

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,12 +1,79 @@
 import React from "react";
-
+import { useState } from "react";
+import "../styles/form.css";
 
 const Form = () => {
-  //Aqui deberan implementar el form completo con sus validaciones
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (formData.firstName.length < 5) {
+      setErrorMessage("Ingrese un nombre válido");
+      return;
+    }
+
+    if (!validateEmail(formData.email)) {
+      setErrorMessage("Ingrese un email válido");
+      return;
+    }
+
+    console.log("Datos del formulario:", formData);
+
+    const successMessage = `Gracias ${formData.firstName}, te contactaremos lo antes posible vía email.`;
+    alert(successMessage);
+
+    setFormData({
+      firstName: "",
+      lastName: "",
+      email: "",
+    });
+    setErrorMessage("");
+  };
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
 
   return (
     <div>
-      <form>
+      <form className="formulario" onSubmit={handleSubmit}>
+        <h1>Contact me!</h1>
+        <input
+          type="text"
+          name="firstName"
+          placeholder="Nombre"
+          value={formData.firstName}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Apellido"
+          value={formData.lastName}
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+
+        {errorMessage && <div className="error">{errorMessage}</div>}
+        <button type="submit">Enviar</button>
       </form>
     </div>
   );
