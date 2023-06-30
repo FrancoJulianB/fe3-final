77ddc6f21a7dab8efeb4d5beaf2bf3cdb0c7826c
Franco Barrera <frajuba11@gmail.com>
2023-06-30 11:09:32 -0300

    [FEAT] Adding fav button with animation

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import { Link} from 'react-router-dom'
 import { usePage } from "./utils/global.context";
 import axios from "axios";
+import { FaHeart } from 'react-icons/fa';
 
 
 const Card = ({ name, username, id, email }) => {
   const {pageState, pageDispatch} = usePage()
   const urlUser = 'https://jsonplaceholder.typicode.com/users/' + id
 
-  const dentista = {}
   
   const addFav = ()=>{
     
       axios(urlUser)
       .then(res => {
         pageDispatch({type: 'FAV', payload: res.data})
-      })
+      }) 
+  }
+
+  const isFav = () => {
+    return pageState.favs.some((fav) => fav.id === id);
   }
   
   
@@ -34,7 +38,10 @@ const Card = ({ name, username, id, email }) => {
         <img style={{width: "100%", height:"100%"}} src="./images/doctor.jpg" alt={"Dr." + name}/>
         <h5>{email}</h5>
         {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        <button onClick={addFav} className="favButton">Add fav</button>
+        
+        <button onClick={addFav} className={`favButton ${isFav() ? 'favButton--active' : ''}`}>
+          <FaHeart className={`favIcon ${isFav() ? 'favIcon--active' : ''}`} />
+        </button>E
     </div>
   );
 };
