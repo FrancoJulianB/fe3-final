49941d7b1196052975bf1db7ad7fc8a3c8165f5b
Franco Barrera <frajuba11@gmail.com>
2023-06-30 00:06:40 -0300

    [STYLE] Adding css

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,7 +8,12 @@ const Navbar = () => {
     <nav>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
+    <div className='switch-theme'>
+      <input type="checkbox" id="dark-mode" onChange={switchTheme}/>
+      <label for="dark-mode"></label>
+      <div class="background"></div>
+    </div>
+      
     </nav>
   )
 }
